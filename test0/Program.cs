eb0597a09fc7388005d37ba35fd800dd2725f5e8
user3124 <username040424@gmail.com>
2025-04-29 19:40:53 +0700

    Добавил Program.cs

---
+++ test0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Fact(int x)
+            {
+                if (x <= 1) return 1;
+                return x * Fact(x - 1);
+            }
+
+            Fact(6);
+        }
+    }
+}
